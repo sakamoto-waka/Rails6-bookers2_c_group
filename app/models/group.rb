ba4556fb0295a0a3b6c48232029347edb2b314af
sakamoto-waka <wakanmhl@gmail.com>
2022-03-17 04:08:48 +0000

    [update] groups/index page

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,12 @@ class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
   has_one_attached :image
-  
+
   validates :name, presence: true
   validates :introduction, presence: true
+
+  def get_group_image
+    (image.attached?) ? image : 'no_image.jpg'
+  end
+
 end
