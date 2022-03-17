ead09741fabf763c4fd6d7173ea749632a1a4ac6
sakamoto-waka <wakanmhl@gmail.com>
2022-03-17 11:07:20 +0000

    セーブポイント　中間テーブル

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update]
-  
+
   def index
     @groups = Group.all
   end
@@ -12,38 +12,39 @@ class GroupsController < ApplicationController
 
   def new
     @group = Group.new
+    
   end
-  
+
   def create
-    @group = Group.new(group_params)
+    @group = current_user.groups.new(group_params)
     @group.owner_id = current_user.id
     if @group.save
       redirect_to groups_path
     else
       render :new
-    end  
-  end  
+    end
+  end
 
   def edit
   end
-  
+
   def update
     if @group.update(group_params)
       redirect_to groups_path
-    else  
+    else
       render :edit
     end
   end
-  
+
   private
   def group_params
-    params.require(:group).permit(:name, :introduction, :image) 
-  end  
-  
+    params.require(:group).permit(:name, :introduction, :image)
+  end
+
   def ensure_correct_user
     @group = Group.find(params[:id])
-    # unless @group.owner_id == current_user.id
-    #   redirect_to groups_path
-    # end  
-  end  
+    unless @group.owner_id == current_user.id
+      redirect_to groups_path
+    end
+  end
 end
