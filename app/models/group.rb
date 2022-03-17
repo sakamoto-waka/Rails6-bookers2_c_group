161aa906e6069748c50c6e68faab0e4a1b435d9e
sakamoto-waka <wakanmhl@gmail.com>
2022-03-17 03:52:30 +0000

    [update] groups/edit page

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+  has_one_attached :image
   
   validates :name, presence: true
   validates :introduction, presence: true
