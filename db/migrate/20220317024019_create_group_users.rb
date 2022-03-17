d449168a23104e238ac19e2a6be45950de22c15f
sakamoto-waka <wakanmhl@gmail.com>
2022-03-17 03:22:34 +0000

    [add] link_to groups in users/index page

--- db/migrate/20220317024019_create_group_users.rb
+++ db/migrate/20220317024019_create_group_users.rb
@@ -2,7 +2,7 @@ class CreateGroupUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :group_users do |t|
       t.references :user, foreign_key: true
-      t.references :group, foreign_key :true
+      t.references :group, foreign_key: true
 
       t.timestamps
     end
